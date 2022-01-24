a7a0f67d6595c7e3b1ac44146d5e1f8ea31eff01
dylan <kws60000@naver.com>
2022-01-24 23:48:15 +0900

    create index.page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from '@/views/Index'
 import Login from '@/views/auth/Login'
 import HelloWorld from '@/components/HelloWorld'
 
@@ -8,6 +9,11 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
     {
       path: '/auth/login',
       name: 'Login',
