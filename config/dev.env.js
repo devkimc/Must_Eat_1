8cb83a940b2a29cd17559cf7f862d0a30c1cadb9
franco <kws60000@naver.com>
2022-02-12 23:55:09 +0900

    Hide the api key using dotenv

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,10 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const dotenv = require('dotenv')
+const env = dotenv.config().parsed
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  VUE_APP_LOCAL_API_KEY: JSON.stringify(env.VUE_APP_LOCAL_API_KEY)
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
+const dotenv = require('dotenv')
+const env = dotenv.config().parsed
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_PROD_API_KEY: JSON.stringify(env.VUE_APP_PROD_API_KEY)
 }
