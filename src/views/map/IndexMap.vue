3b7d70779a2bf0544c2c1e10f224afb9d784fb75
franco <kws60000@naver.com>
2022-03-01 16:45:15 +0900

    Add a function to search for a place with keywords

--- src/views/map/IndexMap.vue
+++ src/views/map/IndexMap.vue
@@ -1,29 +1,19 @@
 <template>
   <!-- Map-->
   <div class="indexmap_view">
-    <div id="map"></div>
     <b-card class="card">
       <b-row class="row_input">
         <b-col>
           <b-form-input placeholder='식당 이름' v-model="restNm"></b-form-input>
         </b-col>
       </b-row>
-      <b-row class="row_input">
-        <b-col>
-          <b-form-input placeholder='음식 구분' v-model="foodTp"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="row_input">
-        <b-col>
-          <b-form-input placeholder='대표 메뉴' v-model="MainMn"></b-form-input>
-        </b-col>
-      </b-row>
       <b-row class="row_button">
         <b-col>
-          <b-button block variant="primary">추가하기</b-button>
+          <b-button block variant="primary" @click="keywordSearch()">검색하기</b-button>
         </b-col>
       </b-row>
     </b-card>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -33,29 +23,28 @@ export default {
   name: 'IndexMap',
   data () {
     return {
+      // search
       restNm: '',
-      foodTp: '',
-      MainMn: '',
+      searchOptions: {category_group_code: 'FD6'},
+      resSearch: [],
+      // map
       map: '',
-      markers: [],
       latLng: '',
-      latCdnt: '',
-      lngCdnt: '',
+      // api
       apiKey: '',
       apiUrl: 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=',
       apiServices: '&libraries=services'
     }
   },
-  mounted () {
-    // Set api_key for kakao map
+  created () {
     this.apiKey = process.env.API_KEY_KAKAO_MAP
-
-    // Show kakao map
-    if (window.kakao && window.kakao.maps) {
+  },
+  mounted () {
+    /* global kakao */
+    if (kakao && kakao.maps) {
       this.initMap()
     } else {
       const script = document.createElement('script')
-      /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap)
       script.src = this.apiUrl + this.apiKey + this.apiServices
       document.head.appendChild(script)
@@ -65,44 +54,20 @@ export default {
     initMap () {
       const container = document.getElementById('map')
       const options = {
-        center: new kakao.maps.LatLng(33.497118, 126.530588),
+        center: new kakao.maps.LatLng(37.55108043514493, 126.86483931801229),
         level: 2
       }
       this.map = new kakao.maps.Map(container, options)
-      // 장소 검색 객체를 생성합니다
-      const ps = new kakao.maps.services.Places(this.map)
-
-      // 카테고리로 은행을 검색합니다
-      ps.categorySearch('FD6', this.placesSearchCB, {useMapBounds: true})
-
-      window.kakao.maps.event.addListener(this.map, 'click', mouseEvent => {
-        this.addMarker(mouseEvent.latLng)
-        const latLng = mouseEvent.latLng
-        this.latCdnt = latLng.getLat()
-        this.lngCdnt = latLng.getLng()
-      })
     },
-    addMarker (position) {
-      const marker = new kakao.maps.Marker({
-        position: position
-      })
-      marker.setMap(this.map)
-      this.markers.push(marker)
-    },
-    placesSearchCB (data, status, pagination) {
-      if (status === kakao.maps.services.Status.OK) {
-        for (let i = 0; i < data.length; i++) {
-          this.displayMarker(data[i])
+    keywordSearch () {
+      const places = new kakao.maps.services.Places()
+      const callback = (res, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          console.log(res)
+          this.resSearch = res
         }
       }
-    },
-    displayMarker (place) {
-    // 마커를 생성하고 지도에 표시합니다
-      const marker = new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(place.y, place.x)
-      })
-      marker.setMap(this.map)
-      this.markers.push(marker)
+      places.keywordSearch(this.restNm, callback, this.searchOptions)
     }
   }
 }
